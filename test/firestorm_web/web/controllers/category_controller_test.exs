bf837d028271d9bbbb18b4b4b17cbb2d2c759a7e
Dave Aronson <davearonson@users.noreply.github.com>
2017-08-05 16:42:20 -0400

    get category controller test working

--- test/firestorm_web/web/controllers/category_controller_test.exs
+++ test/firestorm_web/web/controllers/category_controller_test.exs
@@ -14,7 +14,7 @@ defmodule FirestormWeb.Web.CategoryControllerTest do
 
   test "lists all entries on index", %{conn: conn} do
     conn = get conn, category_path(conn, :index)
-    assert html_response(conn, 200) =~ "Listing Categories"
+    assert html_response(conn, 200) =~ "<ol class=\"category-list\">"
   end
 
   test "renders form for new categories", %{conn: conn} do
